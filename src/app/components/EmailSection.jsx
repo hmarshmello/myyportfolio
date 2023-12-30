ee29cf158f7df20222789b292bbd09a7415c9690
hmarshmello <hs4220@srmist.edu.in>
2023-12-30 13:06:44 +0530

    devs

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -6,6 +6,39 @@ import Link from "next/link";
 import Image from "next/image";
 
 const EmailSection = () => {
+  const [emailSubmitted, setEmailSubmitted] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.email.value,
+      subject: e.target.subject.value,
+      message: e.target.message.value,
+    };
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "/api/send";
+
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: "POST",
+      // Tell the server we're sending JSON.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    };
+
+    const response = await fetch(endpoint, options);
+    const resData = await response.json();
+
+    if (response.status === 200) {
+      console.log("Message sent.");
+      setEmailSubmitted(true);
+    }
+  };
+
   return (
     <section id="contact" className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
@@ -30,13 +63,19 @@ const EmailSection = () => {
             <p><br></br>harshshrivastavagithub@gmail.com</p>
           </Link>
         </div>
-        
       </div>
       <div>
-          <form name = "contact" className="flex flex-col" id = "forms"
-          method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        {emailSubmitted ? (
+          <p className="text-green-500 text-sm mt-2">
+            Email sent successfully!
+          </p>
+        ) : (
+          <form className="flex flex-col" onSubmit={handleSubmit}>
             <div className="mb-6">
-              <label htmlFor="email" className="text-white block mb-2 text-sm font-medium">
+              <label
+                htmlFor="email"
+                className="text-white block mb-2 text-sm font-medium"
+              >
                 Your email
               </label>
               <input
@@ -49,7 +88,10 @@ const EmailSection = () => {
               />
             </div>
             <div className="mb-6">
-              <label htmlFor="subject" className="text-white block text-sm mb-2 font-medium">
+              <label
+                htmlFor="subject"
+                className="text-white block text-sm mb-2 font-medium"
+              >
                 Subject
               </label>
               <input
@@ -62,7 +104,10 @@ const EmailSection = () => {
               />
             </div>
             <div className="mb-6">
-              <label htmlFor="message" className="text-white block text-sm mb-2 font-medium" >
+              <label
+                htmlFor="message"
+                className="text-white block text-sm mb-2 font-medium"
+              >
                 Message
               </label>
               <textarea
@@ -72,11 +117,15 @@ const EmailSection = () => {
                 placeholder="Let's talk about..."
               />
             </div>
-            <button type="submit" className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full">
+            <button
+              type="submit"
+              className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+            >
               Send Message
             </button>
           </form>
-      </div>
+        )}
+        </div>
     </section>
   );
 };
